0e56fd0e98771ada19de4e78ee7e807a78760d5b
CameronGreenUH <47346085+CameronGreenUH@users.noreply.github.com>
2020-10-22 00:06:23 -0500

    First and final commit
    Completed required tasks.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Navigation></Navigation>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation';
+
+export default {
+    name: 'App',
+    components: {
+        Navigation
+    }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  background-color: #EDD2A4;
+  padding-bottom: 30px;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-toolbar app color="brown darken-4" dark>
+    <img class = "logo" src="../assets/tx-logo.png"> <p class = "name"> TEXAS </p> 
+    <router-link to="/">About</router-link> <p>|</p> 
+    <router-link to="/funfact">Fun Facts</router-link>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'Navigation'
+};
+</script>
+
+<style scoped>
+.logo{
+  height: 64px;
+  width: 64px;
+  margin-right: 10px;
+}
+
+.name{
+  margin-right: 10px;
+}
+
+a {
+  font-weight: bold;
+  color: #FFFFFF;
+  padding-right: 10px;
+  padding-left: 10px;
+}
+
+a.router-link-exact-active {
+  color: #BF0A30;
+}
+
+p {
+    color: #FFFFFF;
+}
+
+</style>
